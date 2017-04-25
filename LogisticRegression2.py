642dd50104f574de76684d2907025186961b4d9b
Piyush Ghai <ghai.8@osu.edu>
2017-04-25 18:44:52 -0400

    Modified some files

--- LogisticRegression2.py
+++ LogisticRegression2.py
@@ -2,6 +2,7 @@ from sklearn import linear_model
 
 import load_test_data
 import pre_process
+import roc_curves
 import write_to_csv
 
 X_train, Y_train, X_dev, Y_dev = pre_process.preprocessData('train.csv')
@@ -14,8 +15,8 @@ if __name__ == "__main__":
     p_train = model.predict_proba(X_train)[:, 1]
     # # roc_curves.plotROCCuves(Y_train, p_train, 'roc_lr_train.png', 'Logistic Regression')
     #
-    # p_dev = model.predict_proba(X_dev)[:, 1]
-    # roc_curves.plotROCCuves(Y_dev, p_dev, 'roc_lr_dev.png', 'Logistic Regression')
+    p_dev = model.predict_proba(X_dev)[:, 1]
+    roc_curves.plotROCCuves(Y_dev, p_dev, 'roc_lr_dev.png', 'Logistic Regression')
 
     probs = model.predict_proba(X_test)[:, 1]
     write_to_csv.writeToCSV('preds_lr.csv', probs)
