7ee9e6077c1f84adb8d8c660c5427b53875d741d
Piyush Ghai <ghai.8@osu.edu>
2017-04-24 21:14:42 -0400

    Added pre-process and roc-curve files from the ipynb files

---
+++ pre_process.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+def preprocessData(filename):
+	trainfile = open(filename)
+	for line in trainfile:
+    	head = line.rstrip().split(',')
+    	break
+
+    X_train_A = []
+	X_train_B = []
+	y_train = []
+
+	for line in trainfile:
+	    splitted = line.rstrip().split(',')
+	    label = int(splitted[0])
+	    A_features = [float(item) for item in splitted[1:12]]
+	    B_features = [float(item) for item in splitted[12:]]
+	    y_train.append(label)
+	    X_train_A.append(A_features)
+	    X_train_B.append(B_features)
+#     print A_features
+	trainfile.close()
+
+	y_train = np.array(y_train)
+	X_train_A = np.array(X_train_A)
+	X_train_B = np.array(X_train_B)
+
+	for i in range(len(y_train)):
+    	if y_train[i] == 0:
+        	y_train[i] = -1
+
+
+    X_train = transform_features(X_train_A) - transform_features(X_train_B)
+    np.random.seed(1)      #Seed the random number generator to preserve the dev/test split
+	permutation = np.random.permutation(X_train.shape[0])
+	X_train = X_train[permutation,]
+	y_train = y_train[permutation,]
+	X_train = normalize(X_train)
+
+	indices = np.random.permutation(X_train.shape[0])
+	training_idx, test_idx = indices[:5000], indices[5000:]
+	X_t = X_train[training_idx,:]
+	X_dev = X_train[test_idx, :]
+	y_t = y_train[training_idx,]
+	y_dev = y_train[test_idx,]
+
+	X_train = X_t
+	Y_train = y_t
+
+	return X_train, Y_train, X_dev, y_dev
+	
+
+def transform_features(x):
+    return np.log(1+x)
+
+def normalize(X):
+    X_norm = X
+    cols = X.shape[1]
+    for i in range(cols):
+        m = np.mean(X[:, i])
+        std = np.std(X[:, i])
+        X_norm[:, i] = (X[:, i] - m)/std
+    return X_norm
---
+++ roc_curves.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+import matplotlib.pyplot as plt
+
+def plotROCCuves(y_test, y_pred, filename, label):
+	false_positive_rate, true_positive_rate, _ = roc_curve(y_test, y_pred)
+	roc_auc = auc(false_positive_rate, true_positive_rate)
+	roc_label = '{0} {1:0.5f}'.format(label, roc_auc)
+	plt.plot(false_positive_rate, true_positive_rate, label=roc_label, linewidth=2)
+	plt.title('Receiver Operating Characteristic (ROC)')
+	plt.legend(loc='lower right')
+	plt.plot([0, 1], [0, 1], 'k--')     # plot the diagonal
+	plt.xlim([-0.1, 1.2])
+	plt.ylim([-0.1, 1.2])
+	plt.ylabel('True Positive Rate')
+	plt.xlabel('False Positive Rate')
+	plt.savefig(filename)
+	plt.show()
