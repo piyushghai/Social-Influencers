5b820aa58dcc164d12d5bfb711fda6f00221dc1c
Piyush Ghai <ghai.8@osu.edu>
2017-04-24 21:47:45 -0400

    Fixed loading test labels issue

--- load_test_data.py
+++ load_test_data.py
@@ -11,10 +11,8 @@ def loadTestData(filename):
     Y_test = []
     for line in testfile:
         splitted = line.rstrip().split(',')
-        label = int(splitted[0])
         A_features = [float(item) for item in splitted[0:11]]
         B_features = [float(item) for item in splitted[11:]]
-        Y_test.append(label)
         X_test_A.append(A_features)
         X_test_B.append(B_features)
     testfile.close()
@@ -26,11 +24,25 @@ def loadTestData(filename):
     X_test = transform_features(X_test_A) - transform_features(X_test_B)
     X_test = normalize(X_test)
 
-    for i in range(len(Y_test)):
-        if Y_test[i] == 0:
-            Y_test[i] = -1
+    Y_test = getYLabels()
 
     return X_test, Y_test
+
+
+def getYLabels():
+    testRec = open('sample_predictions.csv')
+    for line in testRec:
+        head = line.rstrip().split(',')
+        break
+    yPred = []
+    for line in testRec:
+        splitted = line.rstrip().split(',')
+        val = float(splitted[1])
+        if val >= 0.5:
+            yPred.append(1)
+        else:
+            yPred.append(-1)
+    return yPred
 
 
 def transform_features(x):
